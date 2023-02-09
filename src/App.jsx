3863f04fc3d154c9768f0ae4f72fd1e9e5eb459c
PedroMakengo <pedromakengomiguel@gmail.com>
2023-02-09 23:05:02 +0100

    Primeira etapa concluida

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import styles from "./styles/App.module.css";
 
 export function App() {
   return (
-    <div className="App">
-      <h1></h1>
+    <div className={styles.wrapper}>
+      <div className={styles.boardingPass}>
+        <header>
+          <span>Cartão de embarque</span>
+        </header>
+        <article className={styles.ticket}>
+          <h1>Ticket</h1>
+        </article>
+        <footer>
+          <p>
+            Qualquer problema procure o balcão de atendimento da sua companhia
+            aérea
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
